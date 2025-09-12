e63900060c84d530f41352702fc1bbd19e5821c8
andy_wendy <alexkens93@gmail.com>
2025-09-12 10:59:46 +0200

    alter names, first jquery tries

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
-$( "a" ).on("click", function() {
+// header
 
-    alert( "Thanks for visiting!" );
+const x = $( "#logo" ).html();
+console.log(x);
 
+
+$("#sidebar-toggle").on("click", function() {
+
+    $( "#aside" ).load("aside.txt");
+
+    // $("aside").css("display", "block");
 });

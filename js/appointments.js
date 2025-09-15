1d39be3f8228ed99d1df71092451587ddc13849a
andy_wendy <alexkens93@gmail.com>
2025-09-15 23:57:54 +0200

    finish appointments

---
+++ js/appointments.js
@@ -0,0 +1,31 @@
+$.when( $.ready ).then(async function() {
+
+    const url = "./media/content.json";
+    const response = await fetch(url);
+    const content = await response.json();
+
+    const gigs = ["current", "archive"];
+    for(let key in gigs) {
+
+        console.log(key);
+        console.log(gigs[key]);
+
+        const contentJSON = content["gigs"][gigs[key]];
+        const contentARRAY = [];
+        for(const k in contentJSON) {
+            if(contentJSON.hasOwnProperty(k)) {
+                contentARRAY.push(contentJSON[k]);
+            }
+        }
+
+        for(let index=contentARRAY.length-1; index >= 0; index--) {
+            const gig = contentARRAY.at(index);
+            const date = gig["date"];
+            const address = gig["address"];
+
+            const appointment = `<div class="bg-slate-300 w-120 rounded-xl p-6  text-slate-700 shadow-md"><h3 class="font-bold">${date}</h3><p>${address}</p></div>`;
+
+            $( `.${gigs[key]}-container` ).append(appointment);
+        }
+    }
+});

533fb1416ede9bc73ae8b26ed703ffcb93069b98
andy_wendy <alexkens93@gmail.com>
2025-09-18 23:44:26 +0200

    member carousel progress

--- js/index.js
+++ js/index.js
@@ -11,21 +11,9 @@ $.when( $.ready ).then(async function() {
     $( "#hero-carousel" ).prop("src", firstImg);
     $( "#hero-carousel" ).css("animation", "fadein 3s");
 
-    // hero carousel
     heroCarouselTrigger();
 
-    // member
-    const memberName = content["profiles"]["0"]["name"];
-    const memberInstrument = content["profiles"]["0"]["instrument"];
-    const memberBio = content["profiles"]["0"]["bio"];
-    const memberImg = content["profiles"]["0"]["img"];
-
-    $( ".member-name" ).text(memberName);
-    $( ".member-instrument" ).text(memberInstrument);
-    $( ".member-text" ).text(memberBio);
-    $( ".member-img" ).prop("src", memberImg);
-
-    // member carousel
+    memberCarouselInit(content);
     memberCarouselTrigger(content);
 });
 
@@ -77,22 +65,42 @@ function heroCarouselL() {
 
 // actual carousel functionality
 function heroCarouselTrigger() {
+    CarouselClickListener("#hero-wrapper");
     setInterval(heroCarouselR, 8000);
+}
+
+// member carousel init
+function memberCarouselInit(content) {
+    const memberName = content["profiles"]["0"]["name"];
+    const memberInstrument = content["profiles"]["0"]["instrument"];
+    const memberBio = content["profiles"]["0"]["bio"];
+    const memberImg = content["profiles"]["0"]["img"];
+
+    $( ".member-name" ).text(memberName);
+    $( ".member-instrument" ).text(memberInstrument);
+    $( ".member-text" ).text(memberBio);
+    $( ".member-img" ).prop("src", memberImg);
 }
 
 // member carousel functionality
 function memberCarouselTrigger(content) {
+
+    CarouselClickListener("#members");
+
     var memberIndex = 0;
     setInterval(() => {
         memberIndex = memberCarousel(memberIndex, content);
-    }, 6000);
+    }, 4000);
 }
 
 function memberCarousel(memberIndex, content) {
+
+    let previousIndex = memberIndex;
     memberIndex++;
-    if(memberIndex >= 4) {
-        memberIndex = 0
+    if(memberIndex > 3) {
+        memberIndex = 0;
     }
+
     const memberName = content["profiles"][memberIndex]["name"];
     const memberInstrument = content["profiles"][memberIndex]["instrument"];
     const memberBio = content["profiles"][memberIndex]["bio"];
@@ -103,7 +111,32 @@ function memberCarousel(memberIndex, content) {
     $( ".member-text" ).text(memberBio);
     $( ".member-img" ).prop("src", memberImg);
 
-    $( `.${memberIndex}` ).addClass("text-white");
+    
+
+    const wrapper = $( ".members-wrapper" );
+    wrapper.removeClass("opac");
+    const offsetWidth = wrapper.offsetWidth; // force reflow
+    wrapper.addClass('opac');
+
+
+    $( `#member-carousel .${memberIndex}` ).addClass("text-white");
+    $( `#member-carousel .${previousIndex}` ).removeClass("text-white");
 
     return memberIndex;
+}
+
+async function memberCarouselStep(direction) {
+    // content.json
+    const url = "./media/content.json"
+    const response = await fetch(url);
+    const content = await response.json();
+
+
+
+}
+
+function CarouselClickListener(carouselId){
+    $( carouselId ).on("click", function() {
+        this.style.backgroundColor = "green";
+    });
 }
